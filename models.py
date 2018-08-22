1c401d886b879cfaf0f218a5321edafaeda47ae7
zylixn <1164336592@qq.com>
2018-08-22 23:17:04 +0800

    教学博客

--- models.py
+++ models.py
@@ -1,4 +1,5 @@
 from exts import db
+import datetime
 
 class User(db.Model):
     __tablename__ = 'users'
@@ -17,5 +18,24 @@ class Questions(db.Model):
     content = db.Column(db.String(128))
     author_id = db.Column(db.Integer,db.ForeignKey('users.id'))
     author = db.relationship('User',backref="questions")
+    createtime = db.Column(db.DateTime,default=datetime.datetime.now)
 
+    def __init__(self,title,content,author_id):
+        self.title     = title
+        self.content   = content
+        self.author_id = author_id
 
+class Reply(db.Model):
+    __tablename__ = "replys"
+    id = db.Column(db.Integer,primary_key=True,autoincrement=True)
+    content = db.Column(db.String(128))
+    author_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    author = db.relationship('User', backref="replys")
+    question_id = db.Column(db.Integer, db.ForeignKey('questions.id'))
+    question = db.relationship('Questions', backref=db.backref('replys',order_by = id.desc()))
+    createtime = db.Column(db.DateTime, default=datetime.datetime.now)
+
+    def __init__(self,content,author_id,question_id):
+        self.content = content
+        self.author_id = author_id
+        self.question_id = question_id
